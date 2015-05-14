046387cb3fec0e9a3ce53d48517b292a575328ce
VovaT <technukrinbank@gmail.com>
2015-05-14 18:39:11 +0300

    test Templates continue

---
+++ app/scalix/ObjectList.py
@@ -0,0 +1,14 @@
+class Test_m(object):
+    def __init__(self, ll):
+        self._item = ll
+
+    @property
+    def name(self):
+        return str(self._item) + 'test'
+
+    @property
+    def prop(self):
+        ret = []
+        for a in range(5):
+            ret.append(str(self._item) + 'Proper#' + str(a))
+        return ret
--- app/views.py
+++ app/views.py
@@ -1,10 +1,15 @@
 __author__ = 'vth'
 from app import app
 from flask import render_template
+from scalix.ObjectList import *
+
+
+
 
 @app.route('/')
 @app.route('/index')
 def index():
     #return "Hello, World!"
-    return render_template('main.html')
+    doubles = list(Test_m(2 * n) for n in range(50))
+    return render_template('main.html', my_arr = doubles)
     #return render_template('object_list.html')
